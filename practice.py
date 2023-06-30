911643d438a6713fb572be7b2337d52e3dc7751b
Md Mehrab Hossain <mehrab.it99@gmail.com>
2023-06-30 15:31:01 +0300

    init variable, data and  operators

---
+++ practice.py
@@ -0,0 +1,47 @@
+# Variables and Data-types in Python
+name1 = "Hossain Md Mehrab"
+number1 = 3.1415
+number2 = True
+number3 = 2+3j
+
+print (name1)
+type(name1)
+
+print (number1)
+type(number1)
+
+print (number2)
+type(number2)
+
+
+print (number3)
+type(number3)
+
+
+# Operators in Python
+a = 10
+b = 20
+## + - * / Arithmetic
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+## > < == != Relational
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+## & (AND), | (OR) Logical(Bitwise) 
+a = True
+b = False
+print(a&b)
+print(a|b)
+
+# Tokens in Python
+## Keywords
+
+## Identifiers
+## Instance 
+## Operators 
+
+
